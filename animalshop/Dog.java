f765ddb80ce59addfe622164c604fd68e17fd382
Magma4 <youngmagma@protonmail.com>
2024-02-23 23:39:34 +0000

    Constructors

--- animalshop/Dog.java
+++ animalshop/Dog.java
@@ -15,8 +15,36 @@ public class Dog {
         public Dog(String name, String breed, String barknoise, double weight){
             this.name = name;
             this.breed = breed;
-            this.barkNoise = barknoise;
+            barkNoise = barknoise;
             this.weight = weight;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public String getBreed(){
+            return breed;
+        }
+
+        public double getWeight(){
+            return weight;
+        }
+
+        public void setName(String name){
+            this.name = name;
+        }
+
+        public void setBreed(String breed){
+            this.breed = breed;
+        }
+
+        public void setWeight(double weight){
+            this.weight = weight;
+        }
+
+        public void bark(){
+            System.out.println(barkNoise);
         }
 
         
