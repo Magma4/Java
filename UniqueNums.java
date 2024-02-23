a23e2f157ff58adec2c3c825db1efa4ec9a2b16d
Magma4 <youngmagma@protonmail.com>
2024-02-23 00:38:40 +0000

    unique numbers

--- UniqueNums.java
+++ UniqueNums.java
@@ -1,25 +1,24 @@
 import java.util.Scanner;
 
-public class UniqueNumbers {
-    public static void main(String[] args) {
+public class UniqueNums{
+    public static void main(String[] args){
         int[] numbers = new int[5];
 
-        Scanner in = new Scanner(System.in);
         int num = 0;
         int numValues = 0;
         boolean valid = true;
-        
 
-        while(numValues<numbers.length){
+        Scanner in = new Scanner(System.in);
+        
+        while(numValues < numbers.length){
             do{
                 valid = true;
-                System.out.println("Please enter a valid unique value: ");
+                System.out.println("Please enter a valid unique number: ");
                 num = in.nextInt();
-                for(int i=0; i<numValues;i++);
-                {
-                    int i;
+
+                for(int i=0;i<numValues;i++){
                     if(num == numbers[i]){
-                        System.out.println("number already exists");
+                        System.out.println("Number already exists");
                         valid = false;
                         break;
                     }
@@ -27,8 +26,13 @@ public class UniqueNumbers {
             }while(!valid);
             numbers[numValues] = num;
             numValues++;
-        }
 
-        
+            
+        }
+        in.close();
+            for(int numV: numbers){
+                System.out.println(numV);
+            }
+
     }
 }
--- ValueChecker.java
+++ ValueChecker.java
@@ -5,19 +5,20 @@ public class ValueChecker{
         Scanner in = new Scanner(System.in);
 
         int intVal;
-
+        do{
         System.out.println("Please enter an integer value: ");
         intVal = in.nextInt();
-
-        if(intVal == 7){
-            System.out.println("That's lucky!");
-        } 
-        else if(intVal == 13){
-            System.out.println("That's unlucky!");
-        }
-        else{
-            System.out.println("That number is neither lucky nor unlucky!");
-        }
+        
+            if(intVal == 7){
+                System.out.println("That's lucky!");
+            } 
+            else if(intVal == 13){
+                System.out.println("That's unlucky!");
+            }
+            else if(intVal != 0){
+                System.out.println("That number is neither lucky nor unlucky!");
+            }
+        }while(intVal != 0);
         in.close();
     }
 }
