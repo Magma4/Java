c8a9d2fce2d9bad564de1cabcbccf85a2f5918e5
Magma4 <youngmagma@protonmail.com>
2024-02-24 23:50:18 +0000

    Done with the dog class

--- animalshop/Dog.java
+++ animalshop/Dog.java
@@ -3,17 +3,44 @@ package animalshop;
 public class Dog extends Animal {
         private String barkNoise = "Woof";
         private double weight;
+        private String breed;
 
         public Dog(String name, String breed, String colour, double weight){
-            super(name, breed, colour);
+            super(name, colour);
+            this.breed = breed;
             this.weight = weight;
         }
 
         public Dog(String name, String breed, String barknoise, String colour, double weight){
-            super(name, breed, colour);
+            super(name, colour);
             this.barkNoise = barknoise;
+            this.breed = breed;
             this.weight = weight;
         }
         
+        public String getBreed(){
+            return breed;
+        }
+
+        public void setBreed(String breed){
+            this.breed = breed;
+        }
+
+        public String getBarknoise() {
+            return barkNoise;
+            
+        }
+
+        public void setBarknoise(String barknoise){
+            this.barkNoise = barknoise;
+        }
+
+        public double getWeight(){
+            return weight;
+        }
+
+        public void setWeight(double weight){
+            this.weight = weight;
+        }
     
 }
