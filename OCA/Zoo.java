fd2e03cc3698759143e7723143f4dfd8c5862986
Magma4 <youngmagma@protonmail.com>
2024-03-04 00:27:58 +0000

    Go hard

---
+++ OCA/Zoo.java
@@ -0,0 +1,15 @@
+/**
+ * @author Magma
+ * Java Doc comment
+ * 
+ */
+
+package OCA;
+
+public class Zoo {
+    public static void main(String args[]) {
+        System.out.println("Hi, i am main method");
+        
+
+    }
+}
